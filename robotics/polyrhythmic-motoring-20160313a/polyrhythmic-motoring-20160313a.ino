eec07e3321268e0b0346cfe4e898e9f1266fc6d2
Jesse Stiles <jessestiles@Mac-Pro.wv.cc.cmu.edu>
2016-03-16 10:25:35 -0400

    cleaned up root dir, added polyrhythmic motor arduino sketch

---
+++ robotics/polyrhythmic-motoring-20160313a/polyrhythmic-motoring-20160313a.ino
@@ -0,0 +1,63 @@
+
+
+#include <Wire.h>
+#include <Adafruit_MotorShield.h>
+#include "utility/Adafruit_MS_PWMServoDriver.h"
+
+Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
+Adafruit_DCMotor *m1 = AFMS.getMotor(1);
+Adafruit_DCMotor *m2 = AFMS.getMotor(4);
+
+unsigned long timenow;
+
+unsigned long time1 = 0;
+unsigned long time2 = 0;
+int tick1 = 400;
+int tick2 = 600;
+int release = 20;
+
+void setup() {
+  Serial.begin(9600);           // set up Serial library at 9600 bps
+  Serial.println("Adafruit Motorshield v2 - DC Motor test!");
+
+  AFMS.begin();  // create with the default frequency 1.6KHz
+
+}
+
+void playMotors(){
+    if (timenow - time1 >= tick1) {
+    time1 = timenow;
+    m1->run(FORWARD);
+    m1->setSpeed(155);
+    }  // turn on motor1 after interval
+
+
+  if (timenow - time1 >= release) {
+    m1->run(RELEASE);
+    } // turn off motor1 20 ms after it went on
+
+
+  if (timenow - time2 >= tick2) {
+    time2 = timenow;
+    m2->run(FORWARD);
+    m2->setSpeed(155);
+    }  // turn on motor1 after interval
+
+
+  if (timenow - time2 >= release) {
+    m2->run(RELEASE);
+    } // turn off motor1 20 ms after it went on
+}
+
+void loop() {
+
+timenow = millis();
+
+for (release >= 20; release <= 400; release = release + 10){
+  playMotors();
+}
+for (release <= 400; release >= 20; release = release - 10){
+  playMotors();
+}
+
+}
